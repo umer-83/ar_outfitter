0d1f29ae4f64f28eee9d695c7a170c9806efa991
umer-83 <umerg95@gmail.com>
2022-06-19 22:48:09 +0500

    home done

--- arboa/lib/utils/data.dart
+++ arboa/lib/utils/data.dart
@@ -1,21 +1,23 @@
 // ignore_for_file: non_constant_identifier_names
 
 List<Map<String, dynamic>> data = [
-  /*{
+  {
     "id": '1',
-    "company_name": "Outfitters",
+    "company_name": "FUROR",
     "services": "Shirts, T-shirts, Jeans",
-    "cover_image":"images/out1.jpg",
-    "address": "7 College Rd, F-7 Markaz F 7 Markaz F-7, Islamabad, Islamabad Capital Territory",
-    "rating": 4.5,
+    "cover_image":
+        "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2F09cf88bd-55f3-49f1-b535-b303d3afd711.png?alt=media&token=4e99e857-20ba-43d7-95ef-ea3f5ae4a104",
+    "address":
+        "Emporium Mall, G-42, Ground Floor, Johar Town، Commercial Area Phase 2 Johar Town, Lahore, Punjab 54000",
+    "rating": 4.0,
     "description":
-        "Outfitters Stores (Pvt.) Ltd., is the parent company of Pakistan’s leading fashion brands Outfitters and Ethnic with omnichannel presence, 120+ stores across 20 cities.",
-    "phone": "(051) 2653114",
+        "Finest collection of Men Casual Shirts, Polo Shirts, Graphic Art T-Shirts, Men's Jeans, Shoes, Bags, Belts, Bracelets, Hoodies, Jackets, Sweat Shirts. ",
+    "phone": "0382929191",
     "portfolio": [
-      "images/o2.jpg",
-      "images/o3.jpg",
-      "images/o4.jpg",
-      "images/o5.jpg",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2Fee732f6a-b84a-4123-81c8-b0b4a70ad04d.jpg?alt=media&token=e3f0ad18-94fb-4986-97f9-ea38c0bed84a",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2F72b86e94-b00a-4e02-87d2-d08cb13c1ab0.jpg?alt=media&token=60a3be78-3c2b-4e33-bedb-15b1f6a95d39",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2Fc6129973-78f7-4419-808d-e8f58289ebf9.jpg?alt=media&token=3319c2a7-f9e3-457f-8785-05bc8a043680",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2F864a6cb9-d121-48e2-b537-82d3a4539668.jpg?alt=media&token=381b1b7c-9a0d-4fae-b6cf-b68870ab33ee",
     ]
   },
   {
@@ -23,79 +25,95 @@ List<Map<String, dynamic>> data = [
     "company_name": "Cougar",
     "services": "Sewing, Stitching",
     "cover_image":
-        "images/c1.jpg",
-    "address": "P352+327, The Centaurus Mall,Second Floor, Jinnah Avenue, F-8/4 F-8, Islamabad, Islamabad Capital Territory",
+        "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2Fdcc5baa5-d856-4cea-97a8-dab440b4daff.jpg?alt=media&token=9a5730f7-e338-4f44-8804-8c8bc4092dd2",
+    "address":
+        "P352+327, The Centaurus Mall,Second Floor, Jinnah Avenue, F-8/4 F-8, Islamabad, Islamabad Capital Territory",
     "rating": 4.5,
     "description":
         "COUGAR Pakistan's casual wear brand is known for its trendy casual wear for a distinctly urban, outgoing, active and fun loving youth with an informal lifestyle.",
     "phone": "12345678910",
     "portfolio": [
-      "images/c2.jpg",
-      "images/c3.jpg",
-      "images/c4.jpg",
-      "images/c5.jpg",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2Fa09f7212-cda6-4f2b-a3ac-3f217216bede.jpg?alt=media&token=e70b0528-9e49-4a8a-889f-7f272f15122b",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2Fa4fae5ec-5b6d-4e31-bcf5-a27635fc5749.jpg?alt=media&token=63eddcc1-c01a-4ce1-a89b-cf0f7597c951",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2Fb7dc551e-bcb9-4e59-a804-baeb8fcbbd3f.jpg?alt=media&token=bd807c55-adac-4eec-9c34-eb415ad3f9a8",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2F31d4b251-aeb0-408b-8f37-bc97e8369f08.jpg?alt=media&token=2a97fbc2-ef0b-4628-8951-adb0df5a2aa3",
     ]
   },
   {
     "id": '3',
-    "company_name": "Domnark Sewing Center",
-    "services": "Sewing, Stitching, Embroidery",
+    "company_name": "Outfitters",
+    "services": "Shirts, T-shirts, Jeans",
     "cover_image":
-        "images/outfitters.jpg",
-    "address": "Landhi, Karachi",
-    "rating": 4.0,
+        "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2F80421547-1b38-4f07-b6be-c24fe731f729.jpg?alt=media&token=1a9347a8-4e86-4676-9f83-9a3942496245",
+    "address":
+        "7 College Rd, F-7 Markaz F 7 Markaz F-7, Islamabad, Islamabad Capital Territory",
+    "rating": 4.5,
     "description":
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ",
-    "phone": "12345678910",
+        "Outfitters Stores (Pvt.) Ltd., is the parent company of Pakistan’s leading fashion brands Outfitters and Ethnic with omnichannel presence, 120+ stores across 20 cities.",
+    "phone": "(051) 2653114",
     "portfolio": [
-      "images/outfitters.jpg"
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2F7a2dbe5e-9bd1-40b1-af2d-998ff56be897.jpg?alt=media&token=b4fc46e9-7cef-46f2-a077-e1c571dcc426",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2F7c3d34ff-27b1-4577-80a9-a6360cf9c89c.jpg?alt=media&token=14133aea-a59e-4c83-b7f2-515638a3851c",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2F26197bea-4ebe-4ff1-9991-d2c0e3a8d97b.jpg?alt=media&token=b3527340-2224-4b9f-8481-ce148f730b4d",
     ]
   },
   {
     "id": '4',
-    "company_name": "Sara's Embroidery",
-    "services": "Embroidery",
+    "company_name": "J. Junaid Jamshed",
+    "services": "Shalwar, Kameez, Sherwani",
     "cover_image":
-        "images/outfitters.jpg",
-    "address": "Gulshan-e-Iqbal, Karachi",
+        "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2F173c13f9-e665-422a-b43d-ef25d702399f.png?alt=media&token=d72a5774-f361-4a48-822a-84b7ee977607",
+    "address":
+        "G5C4+MVM, Giga Mall, Sector F DHA Phase II, Islamabad, Islamabad Capital Territory",
     "rating": 5.0,
-    "phone": "12345678910",
+    "phone": "12579999999",
     "description":
-        "A professional tailoress working from last 3 years in this domain. I have sewed boutique clothes, Branded Shalwar, Fashionable Kameez, Kurtis with new designs of stitching on clothes.",
+        "Welcome to J. Junaid Jamshed Pakistan & Global online store. Shop online for the latest trends of Women unstitched, Kurti, Stitched suits.",
     "portfolio": [
-      "",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2Fae345cb4-c700-4df5-a4a4-b2dc5073c8ac.jpg?alt=media&token=08d3ff27-7956-4baf-b406-896a761d4eba",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2Fa3329593-51cd-4029-97ae-c798eccfba94.jpg?alt=media&token=bab4bb07-6eee-4faf-877a-0833160ebbf8",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2F13c41a53-83ea-4701-9523-c05ff6730ac3.jpg?alt=media&token=cd5ca37a-749f-4654-8f97-dbffb262e293",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2Ffec63277-65b0-4299-8628-af1714fe6cc5.jpg?alt=media&token=4d215fb5-7fc6-422c-b867-1c25576dfecd",
     ]
   },
   {
     "id": '5',
-    "company_name": "Bilquies Stitching Center",
-    "services": "Sewing, Stitching",
+    "company_name": "Nike",
+    "services": "Shirts, T-shirts, Trousers, Shorts, Shoes",
     "cover_image":
-        "images/outfitters.jpg",
-    "address": "Phase IV, Clifton, Karachi",
-    "rating": 3.5,
-    "phone": "12345678910",
+        "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2F43ccbc2d-24b1-4007-a2a9-367f9fa6736e.jpg?alt=media&token=0f6f96c0-2e0d-4869-b09d-ae188b84b767",
+    "address":
+        "17, Ground Floor، Safa Gold Mall, Shop No.14، 7 College Rd, F-7 Markaz F 7 Markaz F-7, Islamabad, Islamabad Capital Territory",
+    "rating": 4.5,
+    "phone": "+1368548999",
     "description":
-        "I am Bilques. I can sew and stitch different types of clothes and make new designs from it.",
+        "Nike delivers innovative products, experiences and services to inspire athletes.",
     "portfolio": [
-      "images/outfitters.jpg"
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2F3a2c4ec2-d891-4d33-a1b5-4f33b2f68592.jpg?alt=media&token=a86f1d9d-e954-4d8b-83c6-4ac52c0a7a27",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2F43e4b9a6-48f6-41cc-b9a7-614d4d357709.jpg?alt=media&token=26c1bc6f-f3c2-49e9-9d2d-d218d505a9de",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2F3f8ed5f5-9c48-475c-a182-5e453d0d77b2.jpg?alt=media&token=7a1a2d81-8e3c-4a6d-afb4-fd34b82711f6",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2F6d3e032e-e61d-4620-9806-674f7298088b.jpg?alt=media&token=f8aa6eac-df58-40c3-a024-ab754f2ba2e5",
     ]
   },
   {
     "id": '6',
-    "company_name": "BB Sewing",
+    "company_name": "LEVI'S",
     "services": "Sewing",
     "cover_image":
-        "images/outfitters.jpg",
-    "address": "Gulshan-e-Iqbal, Block 13-B, Karachi",
+        "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2Fa49b73eb-d484-4721-b402-79611ffbeeb9.jpg?alt=media&token=c72a3afe-c170-4d4e-a992-51f156670b32",
+    "address":
+        "Ground Floor, The Centaurus Mall, Jinnah Avenue, F 8/4 F-8, Islamabad, Islamabad Capital Territory 44000",
     "rating": 4.5,
     "description":
-        "I am working from 2 years in this domain. I can sew men clothes including shirts, pants, Kurta, Pajama, shalwars, and kameez.",
-    "phone": "12345678910",
+        "The official Levi's® US website has the best selection of Levi's® jeans, jackets, and clothing for men, women, and kids. Shop the entire collection today.",
+    "phone": "+48399272829",
     "portfolio": [
-      "images/outfitters.jpg"
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2Fb58b0ac0-c950-4a9b-9c12-09854308eedd.jpg?alt=media&token=4f0646d9-a45b-4d96-8855-0c3ecc533efc",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2F9610a840-fd10-441d-996f-c8407d25939f.jpg?alt=media&token=b34cce52-3cf8-45ec-9692-7ffab9d67512",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2Fd057871c-d97a-40ee-b7d5-105821176497.jpg?alt=media&token=7fff1620-05cf-48e4-9d8c-706f4abeaaf8",
+      "https://firebasestorage.googleapis.com/v0/b/ar-outfitter-app.appspot.com/o/appImages%2Fad7576df-89fa-4559-ad48-42d548a03352.jpg?alt=media&token=d07ac427-5943-43d2-8036-ba6b41438190",
     ]
-  }*/
+  }
 ];
 
 /*class ListOfAllData {
