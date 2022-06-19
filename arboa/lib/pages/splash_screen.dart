1a9202d28f3142ab5f6d74f16791ee16fe7f11b0
Muhammad farhan <mmfarhan120@gmail.com>
2022-06-19 17:26:28 +0500

    feat/splash screen

---
+++ arboa/lib/pages/splash_screen.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+import 'package:ar_outfitter/main.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class SplashScreen extends StatefulWidget {
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(Duration(seconds: 10), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Routes()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "images/ARBOA-logos_transparent.png",
+              height: 240,
+              width: 240,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                "Augmented Reality Based Outfitter Application",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "All in one!",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const SpinKitFadingCircle(
+              duration: Duration(seconds :10),
+              color: Colors.black,
+              size: 50.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
